bbcdc29e335b65dc8f158114fcd1408246708d24
Koichi Tamura <hohehohe2@gmail.com>
2017-06-23 02:51:12 +0900

    run.bat update

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export TIME_KEEPER_GIT_REPOSITORY=/Users/kotamura/myGitRepos/nody/timeKeeperTestData
+export TIME_KEEPER_GIT_REPOSITORY=/Users/kotamura/myGitRepos/timeKeeper/timeKeeperTestData
 
-export PYTHONPATH=/Users/kotamura/Downloads/Qt.py-master:/Users/kotamura/myGitRepos/nody
+export PYTHONPATH=/Users/kotamura/Downloads/Qt.py-master:/Users/kotamura/myGitRepos/timeKeeper
 
-/opt/local/bin/python2.7 main.py
+/opt/local/bin/python2.7 /Users/kotamura/myGitRepos/timeKeeper/main.py
